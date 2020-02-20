f7498fa5a067492eabd102a2bfb3ff70dfcb0506
Matthew Baker <mattbaker2281@gmail.com>
2020-02-20 11:21:36 -0600

    commented code and made readme

--- src/scripts/articles/newsDOM.js
+++ src/scripts/articles/newsDOM.js
@@ -1,3 +1,8 @@
+/*
+    Written by Matthew B
+    Allows the articles to be displayed on the DOM
+*/
+
 import newsFactory from "./newsCreateHTML.js"
 
 const container = document.querySelector("#containerOne")
@@ -5,6 +10,7 @@ const container = document.querySelector("#containerOne")
 const renderNewsEntries = (entries) => {
     container.innerHTML = ""
 
+    // Loops through each news article and puts each one into the container on the DOM
     entries.forEach(entry => {
         const newsHTML = newsFactory.newsFactory(entry)
         container.innerHTML += newsHTML
